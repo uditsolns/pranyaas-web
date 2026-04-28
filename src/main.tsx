a8e95f83976e363ed8872ff9e9af88f53147801a
anshucodeslife <itsanshursingh@gmail.com>
2026-04-28 13:57:49 +0530

    /Pranyaas  upload build

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Pranyaas">
     <AuthProvider>
       <App />
     </AuthProvider>
